4a3fc1830768c3e144b39f1563eeccf561d74d1f
Mohamed76300 <150445798+Mohamed76300@users.noreply.github.com>
2023-12-10 17:02:18 +0100

    code de la bataille fini

--- solitaire fini.py
+++ solitaire fini.py
@@ -1,110 +1,96 @@
+# Créé par basri, le 10/12/2023 en Python 3.7
 import random
-VALEURS = ['','', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As']
+from collections import deque
+# Définition des valeurs et couleurs des cartes
+VALEURS = ['', '', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As']
 COULEURS = ['', 'pique', 'coeur', 'carreau', 'trefle']
-
-
+# Définition de la classe Carte
 class Carte:
-    """Initialise couleur (de 1 à 4), et valeur (de 2 à 14)"""
-
     def __init__(self, couleur, valeur):
         self.couleur = couleur
         self.valeur = valeur
 
     def get_nom(self):
-        """Renvoie le nom de la Carte As, 2, ... 10, Valet, Dame, Roi"""
         return VALEURS[self.valeur]
 
     def get_couleur(self):
-        """Renvoie la couleur de la Carte (parmi pique, coeur, carreau, trefle)"""
         return COULEURS[self.couleur]
 
+    def get_valeur(self):
+        return self.valeur
+# Définition de la classe PaquetDeCarte
 class PaquetDeCarte:
-    """Initialise un paquet de cartes, avec un attribut contenu, de type list, vide"""
-
     def __init__(self):
         self.contenu = []
 
     def remplir(self):
-        """Remplit le paquet de cartes : en parcourant les couleurs puis les valeurs"""
+        # Création d'un paquet de cartes avec l'ensemble des combinaisons de valeurs et de couleurs possibles
         self.contenu = [Carte(couleur, valeur) for couleur in range(1, 5) for valeur in range(2, 15)]
-
-    def get_carte_at(self, pos):
-        """Renvoie la Carte qui se trouve à la position donnée"""
-        if 0 <= pos < 52:
-            return self.contenu[pos]
-
-PaquetDeDistribution=PaquetDeCarte()
-PaquetDeDistribution.remplir()
-
+        random.shuffle(self.contenu)
+# Définition de la classe Joueur
 class Joueur:
+    def __init__(self):
+        self.PaquetJoueur = deque() # On va Utiliser  une  file pour représenter le paquet du joueur
 
-    def distribuerJoueur(self):
-        if len(PaquetDeDistribution)==52:
-            for i in range(27,52,-1):
-                PaquetJoueur.append(PaquetDeDistribution.pop(random.randint(1,i)))
-        else:
-            for i in range(1,26,-1):
-                PaquetJoueur.append(PaquetDeDistribution.pop(random.randint(1,i)))
-
-    def get_carte_at_Joueur(self, pos):
-        if 0 <= pos < 26:
-            return self.PaquetJoueur[pos]
-
-
-PaquetJoueur1=Joueur()
-PaquetJoueur2=Joueur()
-PaquetJoueur1.distribuerJoueur()
-PaquetJoueur2.distribuerJoueur()
-TapisJoueur1=Tapis()
-TapisJoueur2=Tapis()
-carte_en_jeu_joueur1=[]
-carte_en_jeu_joueur2=[]
+    def distribuerJoueur(self, PaquetDeDistribution):
+        # On Distribue la moitié du paquet au joueur
+        self.PaquetJoueur.extend(PaquetDeDistribution.contenu[:26])
+        PaquetDeDistribution.contenu = PaquetDeDistribution.contenu[26:]
+# Définition de la classe Tapis
 class Tapis:
     def __init__(self):
-     self.cartes_en_jeu = [] #attribut cartes_en_jeu qui est une liste vide. il  stocke les cartes posées sur le tapis
+        self.cartes_en_jeu = deque() # File pour les cartes en jeu
+        self.paquet_bataille = deque() # Pile pour les cartes dans la  bataille
 
-    def poser_carte(self): #pose une carte sur le tapis. Prend une carte en argument
-        self.cartes_en_jeu.append()
-        self.pop()
+    def poser_carte(self, joueur):
+        if joueur.PaquetJoueur:
+            return joueur.PaquetJoueur.popleft()  # Prendre la carte du dessus sur le  paquet du joueur
+        else:
+            return None
 
-    def recuperer_cartes(self,Joueurconcerné):#récupére toutes les cartes du tapis
-       Joueurconcerné.append(self[0].pop()
+    def comparer_cartes(self, PaquetJoueur1, PaquetJoueur2):
+        while PaquetJoueur1.PaquetJoueur and PaquetJoueur2.PaquetJoueur:
+            # Comparaison des cartes et gestion de la bataille
+            carte_joueur1 = self.poser_carte(PaquetJoueur1)
+            carte_joueur2 = self.poser_carte(PaquetJoueur2)
 
+            if carte_joueur1 is not None and carte_joueur2 is not None:
+                if carte_joueur1.get_valeur() > carte_joueur2.get_valeur():
+                    self.recuperer_cartes(PaquetJoueur1)
+                else:
+                    self.recuperer_cartes(PaquetJoueur2)
 
-##    def afficher_tapis(self):
-##        ##affiche les cartes actuellement sur le tapis
-##        if self.cartes_en_jeu:
-##            ## sa Vérifie si la liste cartes_en_jeu n'est pas vide
-##            print("Cartes sur le tapis:")
-##            ## indique bien que les cartes sont sur la tapis
-##            for carte in self.cartes_en_jeu:
-##                ##Boucle qui  traverse tt les cartes sur le tapis
-##                print(f"{carte.get_nom()} de {carte.get_couleur()}")
-##                ##  donne  le nom et la couleur de chaque carte sur une nouvelle ligne
-##        else:
-##            ## sinon exécuté si la liste cartes_en_jeu est vide
-##            print("Le tapis est vide.")
-
+    def recuperer_cartes(self, gagnant):
+         # Récupération des cartes dans le paquet du gagnant et la pile de la  bataille
+        self.cartes_en_jeu.extend(gagnant.PaquetJoueur)
+        self.cartes_en_jeu.extend(self.paquet_bataille)
+        gagnant.PaquetJoueur.clear()
+        self.paquet_bataille.clear()
 
 def bataille():
-    while PaquetJoueur1!=[] or PaquetJoueur2!=[]:
-        TapisJoueur1.poser_carte()
-        TapisJoueur2.poser_carte()
-        while TapisJoueur1[0].get_nom()==TapisJoueur2[0].get_nom()
-			for i in range(2):
-                TapisJoueur1.empiler()
-                TapisJoueur2.empiler()
-        if TapisJoueur1[0].get_nom() > TapisJoueur2[0].get_nom()
-            while TapisJoueur1!=[]
-    			TapisJoueur1.recuperer_cartes(PaquetJoueur1)
-    		while TapisJoueur2!=[]
-    			TapisJoueur2.recuperer_cartes(PaquetJoueur1)
-    	else :
-    		while TapisJoueur1!=[]
-    			TapisJoueur1.recuperer_cartes(PaquetJoueur2)
-    		while TapisJoueur2!=[]
-    			TapisJoueur2.recuperer_cartes(PaquetJoueur2)
-    if PaquetJoueur1==[]:
-        return("Joueur 2 vainqueur!")
-    elif PaquetJoueur2==[]:
-        return("Joueur 1 vainqueur!")
+    # Init du paquet de cartes, des joueurs et du tapis
+    PaquetDeDistribution = PaquetDeCarte()
+    PaquetDeDistribution.remplir()
+
+    PaquetJoueur1 = Joueur()
+    PaquetJoueur2 = Joueur()
+
+    PaquetJoueur1.distribuerJoueur(PaquetDeDistribution)
+    PaquetJoueur2.distribuerJoueur(PaquetDeDistribution)
+
+    TapisJeu = Tapis()
+    # Début de la bataille
+    TapisJeu.comparer_cartes(PaquetJoueur1, PaquetJoueur2)
+     # Afficher les résultats
+    if not PaquetJoueur1.PaquetJoueur or not PaquetJoueur2.PaquetJoueur:
+        print("La partie est terminée.")
+
+    if not PaquetJoueur1.PaquetJoueur:
+        print("Joueur 2 vainqueur!")
+    elif not PaquetJoueur2.PaquetJoueur:
+        print("Joueur 1 vainqueur!")
+
+# Test du jeu
+bataille()
+
+
